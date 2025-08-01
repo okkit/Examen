47d9c271328bd5c2b3360bcc436e80ebfd024d1a
okkit <okkit@users.noreply.github.com>
2025-08-01 15:55:14 +0200

    Das Hauptpanel Umstrukturiert

--- src/gui/okkit/HauptPanel.java
+++ src/gui/okkit/HauptPanel.java
@@ -7,35 +7,58 @@ import java.awt.Insets;
 import javax.swing.JPanel;
 
 public class HauptPanel extends JPanel {
+
+	// Wir brauchen später Zugriff auf die Panels. Daher müssen sie Feler sein
+	PanelThemenListe panelThemenListe; // Deklaration des Feldes panelThemenListe
+	PanelNeuesThema panelNeuesThema;
+	PanelAktionen panelAktionen;
+
+	/**
+	 * Konstruktor.
+	 */
 	public HauptPanel() {
+		super();
+		initPanels();
+		addPanels();
+	}
+
+	/**
+	 * Initialisiert die Felder (also die Panels)
+	 */
+	void initPanels() {
+		 panelThemenListe = new PanelThemenListe();
+		 panelNeuesThema = new PanelNeuesThema();
+		 panelAktionen = new PanelAktionen();
+	}
+
+	/**
+	 * Fühgt die Panels hinzu (addet) inkl. Layout
+	 */
+	void addPanels() {
 		setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 
 		// Linkes Panel
-		gbc.gridx = 0; // Spalte 0. x, weil Spalten werden entlang der X-Achse gezähl
-		gbc.gridy = 0; // Zeile 0. y, weil Zeilen werden entlang der Y-Achse gezähl
-		gbc.weightx = 1.0; // Via Experimen eingestellt
-		gbc.weighty = 1.0; // Via Experimen eingestellt
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weightx = 1.0;
+		gbc.weighty = 1.0;
 		gbc.insets = new Insets(15, 10, 0, 5);
-		gbc.fill = GridBagConstraints.BOTH; // Falls in einer Zelle zu viel Platz, stretche den
-											// Inhalt
-		add(new PanelNeuesThema(), gbc);
+		gbc.fill = GridBagConstraints.BOTH;
+		add(panelNeuesThema, gbc);
 
 		// Rechtes Panel
 		gbc.gridx = 1;
-//		gbc.weightx = 1.0; // Überflüssig
 		gbc.insets = new Insets(15, 5, 0, 10);
-		add(new PanelThemenListe(), gbc);
+		add(panelThemenListe, gbc);
 
 		// Unteres Panel
 		gbc.gridx = 0;
 		gbc.gridy = 1;
-		gbc.gridwidth = 2; // weil in der ersten Zeile zwei Spalten sind, "ziehen" wir die zweite
-							// Zeile über zwei Spalten
-//		gbc.weightx = 1.0; // Überflüssig
-		gbc.weighty = 0.0; // Ohne ensteht ein leeres Plats zwischen den oberen Pannel und dem unteren
+		gbc.gridwidth = 2;
+		gbc.weighty = 0.0;
 		gbc.insets = new Insets(5, 10, 10, 10);
 		gbc.fill = GridBagConstraints.HORIZONTAL;
-		add(new PanelAktionen(), gbc);
+		add(panelAktionen, gbc);
 	}
 }
